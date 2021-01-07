0f69fe044efe0162bc999dab8acad0319a7f1cad
LourensT <touwenlourens@gmail.com>
2021-01-07 15:46:58 +0100

    basic implementation, no styling

--- SimulatingScaleFreePercolation.py
+++ SimulatingScaleFreePercolation.py
@@ -11,43 +11,74 @@ import tikzplotlib as plt2tikz
 
 class SimulatingScaleFreePercolation:
 
-    def __init__(self, poisson_mean):
+    def __init__(self):
         #bulkwise sampler is quicker then sampling one by one
-        self.poissonSampler = Distribution(stats.poisson(poisson_mean)) 
-        self.unifSampler = Distribution(stats.unif())
+        #self.poissonSampler = Distribution(stats.poisson(poisson_mean)) 
+        self.unifSampler = Distribution(stats.uniform())
         self.dir = ""
 
     def setOutputFolder(self, dir):
         assert os.path.isdir(dir), "checking if valid folder"
-        self.dir = dir 
+        self.dir = dir
 
-    def generateGraph(self, width, height):
+    '''
+    grid-wise generate vertices (following width, height)
+
+    params:
+    rows: number of rows
+    columns: number of columns
+    lam: weight parameter (avg degrees)
+    tau: powerlaw exponent parameter
+
+    '''
+    def generateGraph(self, columns, rows, lam, tau, alpha):
+        self.lam = lam
+        self.tau = tau
+        self.alpha = alpha
+
         #set new graph
         self.G = nx.Graph()
-        #
-        self.samplePoints()
+        #create new vertices on all 
+        self.setVertices(columns, rows)
+        for vertex in self.vertices:
+            vertex.setWeight(self.sampleWeight())
 
-        # for all nodes
-        for i in self.pointsPositions.keys():
-            self.G.add_node(i)
-        # add all edges between nodes if distance is smaller then radius
-        for pair in itertools.combinations(self.pointsPositions.keys(), r=2):
-            p1 = self.pointsPositions[pair[0]]
-            p2 = self.pointsPositions[pair[1]]
-            if self.calculateDistance(p1, p2) <= self.R:
-                self.G.add_edge(pair[0], pair[1])
+        # add edges
+        for (v1, v2) in itertools.combinations(self.vertices, r=2):
+            if self.sampleEdge(v1, v2):
+                self.G.add_edge(v1.id, v2.id)
 
-    def sampleWeights(self):
-        print("generating weights")
-
-    def setIntegerVertices(self, w, h):
-        print("setting integer positions")
+        print('generated a graph with', str(columns*rows), 'vertices and', self.G.number_of_edges(), 'edges')
+        
+    '''
+    sample a single weight (according to powerlaw distribution)
+    '''
+    def sampleWeight(self):
+        print(self.lam *((self.unifSampler.rvs())**(-1/(self.tau -1))))
+        return self.lam *((self.unifSampler.rvs())**(-1/(self.tau -1)))
+            
+    '''
+    initializes the vertices, in this case according to 
+    '''
+    def setVertices(self, w, h):
+        self.vertices = []
+        id = 0
+        for i in range(w):
+            for j in range(h):
+                self.vertices.append(Vertex(id, i, j))
+                self.G.add_node(id)
+                id += 1
     
     '''
-    decide whether to have edge between 
+    decide wether to add edge or not, based on weights
     '''
-    def sampleEdge(self, weight1, weight2):
-        return True
+    def sampleEdge(self, v1, v2):
+        dist = ((v1.x - v2.x)**2  + (v1.y - v2.y)**2)**(1/2)
+        prob = 1 - np.exp(-self.lam*((v1.weight * v2.weight)**self.alpha) * (1/(dist**(self.alpha*2))) )
+        if self.unifSampler.rvs() <= prob:
+            return True
+        else:
+            return False
 
     '''
     returns euclidian distance between two points
@@ -59,16 +90,17 @@ class SimulatingScaleFreePercolation:
     '''
     output to tex and display graph
     '''
-    def draw(self, fp="", style="", show = False):
+    def draw(self, fp="", style="", show = False, dump=True):
         #get default filepath
         if fp == "":
             fp = datetime.datetime.now().strftime("%Hh%Mm%Ss %d-%m-%Y") +".tikz"
         
         #get the positions of all nodes in euclidian coordinates
-        layout = self.pointsPositions.keys()
+        layout = {v.id : (v.x, v.y) for v in self.vertices}
         #convert the network to tikz
-        network2tikz.plot(self.G, self.dir+fp.replace('.tikz', '.tex'), layout=layout, canvas=(10,10))
-        self.addStyleString(fp, style)
+        if dump:
+            network2tikz.plot(self.G, self.dir+fp.replace('.tikz', '.tex'), layout=layout, canvas=(10,10))
+            self.addStyleString(fp, style)
 
         #display the matplotlib view of the graph
         if show:
@@ -80,7 +112,6 @@ class SimulatingScaleFreePercolation:
     add the given style options to the output.tex at appropriate place
     '''
     def addStyleString(self, fp, style):
-
         #read in the output file and interlace the styling options
         allLines = []
         shouldSave = False #filter to only save the tikz picture
@@ -127,3 +158,23 @@ class SimulatingScaleFreePercolation:
             plt.show()
         
         plt.clf() #clear plot
+
+class Vertex:
+    weight = -1
+
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = x
+        self.y = y
+
+    def setWeight(self, weight):
+        self.weight = weight
+    
+    def getWeight():
+        assert self.weight >= 0, "weight not yet set"
+        return self.weight
+
+if __name__ == "__main__":
+    sim = SimulatingScaleFreePercolation()
+    sim.generateGraph(25, 25, 0.625, 2.5, 3)
+    sim.draw(show=True, dump=False)
